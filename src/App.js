6dc69a5573436ddeb10aa24461a34e0d9a61e2b4
Sebastian <sebastian.stephenson1@gmail.com>
2022-08-27 11:10:04 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import $ from 'jquery';
 
 function App() {
+  const[fahrenheit, setFahrenheit] = useState(0);
+  const[celsius, setCelsius] = useState(0);
+  const[kelvin, setKelvin] = useState(0);
+
+  function test(){
+    let fDeg = $("#fahrenheit").val();
+    
+    setFahrenheit(fDeg);
+    setCelsius((fDeg - 32)/1.8000);
+    setKelvin((fDeg - 32) * (5/9) + 273.15);
+  }
+
+  function test2(){
+    let cDeg = $("#celsius").val();
+    
+    setCelsius(cDeg);
+    setFahrenheit(cDeg * 9/5 + 32);
+    setKelvin((cDeg * 1) + 273.15);
+  }
+
+  function test3(){
+    let kDeg = $("#kelvin").val();
+    
+    setKelvin(kDeg);
+    setFahrenheit((kDeg - 273.15) * 1.8 + 32);
+    setCelsius(kDeg - 273.15);
+  }
+
+  $("#fahrenheit").val(fahrenheit);
+  $("#celsius").val(celsius);
+  $("#kelvin").val(kelvin);
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Temperature Translation</h1>
+
+    <input onChange={test} type='text' name='fahrenheit' id='fahrenheit'></input>
+    <p>°F: {fahrenheit}</p>
+    <br/>
+    <input onChange={test2} type='text' name='celsius' id='celsius'></input>
+    <p>°C: {celsius}</p>
+    <br/>
+    <input onChange={test3} type='text' name='kelvin' id='kelvin'></input>
+    <p>°K: {kelvin}</p>
+    </>
   );
 }
 
