cf9d797836718a1346eeffdea7df9b7a06ac7552
Sebastian <sebastian.stephenson1@gmail.com>
2022-10-19 20:42:42 -0400

    The conversions rounds to the nearest hundredth

--- src/App.js
+++ src/App.js
@@ -11,24 +11,25 @@ function App() {
     let fDeg = $("#fahrenheit").val();
     
     setFahrenheit(fDeg);
-    setCelsius((fDeg - 32)/1.8000);
-    setKelvin((fDeg - 32) * (5/9) + 273.15);
+    setCelsius(((fDeg - 32)/1.8000).toFixed(2));
+    setKelvin(((fDeg - 32) * (5/9) + 273.15).toFixed(2));
+
   }
 
   function test2(){
     let cDeg = $("#celsius").val();
     
     setCelsius(cDeg);
-    setFahrenheit(cDeg * 9/5 + 32);
-    setKelvin((cDeg * 1) + 273.15);
+    setFahrenheit((cDeg * 9/5 + 32).toFixed(2));
+    setKelvin(((cDeg * 1) + 273.15).toFixed(2));
   }
 
   function test3(){
     let kDeg = $("#kelvin").val();
     
     setKelvin(kDeg);
-    setFahrenheit((kDeg - 273.15) * 1.8 + 32);
-    setCelsius(kDeg - 273.15);
+    setFahrenheit(((kDeg - 273.15) * 1.8 + 32).toFixed(2));
+    setCelsius((kDeg - 273.15).toFixed(2));
   }
 
   $("#fahrenheit").val(fahrenheit);
@@ -38,21 +39,21 @@ function App() {
 
   return (
     <>
-    <h1 className='header'>Temperature Conversions</h1>
+      <h1 className='header'>Temperature Conversions</h1>
 
-    <div className='content'>
-      <label>Fahrenheit: </label>
-      <input onChange={test} type='text' name='fahrenheit' id='fahrenheit'></input>
-      <p className='degree'>°F: {fahrenheit}</p>
-      <br/>
-      <label>Celsius: </label>
-      <input onChange={test2} type='text' name='celsius' id='celsius'></input>
-      <p className='degree'>°C: {celsius}</p>
-      <br/>
-      <label>Kelvin: </label>
-      <input onChange={test3} type='text' name='kelvin' id='kelvin'></input>
-      <p className='degree'>°K: {kelvin}</p>
-    </div>
+      <div className='content'>
+        <label>Fahrenheit: </label>
+        <input onChange={test} type='text' name='fahrenheit' id='fahrenheit'></input>
+        <p className='degree'>°F: {fahrenheit}</p>
+        <br/>
+        <label>Celsius: </label>
+        <input onChange={test2} type='text' name='celsius' id='celsius'></input>
+        <p className='degree'>°C: {celsius}</p>
+        <br/>
+        <label>Kelvin: </label>
+        <input onChange={test3} type='text' name='kelvin' id='kelvin'></input>
+        <p className='degree'>°K: {kelvin}</p>
+      </div>
     </>
   );
 }
